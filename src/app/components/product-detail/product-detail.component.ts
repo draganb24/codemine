0e9a65ce2569617719b49267b0dfc17111b9b0e0
draganb24 <nemanjabjelica45@gmail.com>
2024-07-22 11:56:11 +0200

    implemented signals in product detail

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, signal } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
 import { ProductService } from '../../services/product.service';
 import { SpinnerComponent } from '../shared/spinner/spinner.component';
 import { tap, catchError, of, finalize } from 'rxjs';
+import { ProductWithDetails } from '../../interfaces/product-with-details.model';
 
 @Component({
   selector: 'app-product-detail',
@@ -12,10 +13,10 @@ import { tap, catchError, of, finalize } from 'rxjs';
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
   @Input() productId: string;
-  product: any = null;
-  loading: boolean = true;
+  product = signal<ProductWithDetails>(null);
+  loading = signal<boolean>(true);
 
   constructor(
     public activeModal: NgbActiveModal,
@@ -27,17 +28,17 @@ export class ProductDetailComponent {
   }
 
   fetchProductDetails(): void {
-    this.loading = true;
+    this.loading.set(true);
     this.productService.getProductById(this.productId).pipe(
       tap((data) => {
-        this.product = data;
+        this.product.set(data);
       }),
       catchError((error) => {
         console.error('Error fetching product details:', error);
         return of(null);
       }),
       finalize(() => {
-        this.loading = false;
+        this.loading.set(false);
       })
     ).subscribe();
   }
